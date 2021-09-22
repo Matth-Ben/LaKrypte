6b16b20b6da6863cae400b961d972d0534c6c2a8
Matth-Ben <matthias.benoit@outlook.fr>
2021-09-22 14:33:37 +0200

    Init project

---
+++ mu-plugins/require-stylesheets.php
@@ -0,0 +1,9 @@
+<?php
+/*
+MU Plugin: Require Stylesheets
+Description: Importer le style
+Version1: 21 09 2021
+*/
+
+wp_enqueue_style( 'lakrypte-style', get_template_directory_uri() . '/public/css/style.css',false,'1.0','all');
+?>
